79e9c9f82d960c9233f2bf1265311128c21c45ce
HoldRob123 <holden.robinson@comcast.net>
2024-12-05 21:03:01 -0800

    Added more functionality

--- app/src/main/java/com/example/signinsignout/activities/userActivity.java
+++ app/src/main/java/com/example/signinsignout/activities/userActivity.java
@@ -11,8 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.signinsignout.R;
+import com.example.signinsignout.adapters.UsersAdapter;
 import com.example.signinsignout.databinding.ActivityUserBinding;
+import com.example.signinsignout.models.User;
+import com.example.signinsignout.utilities.Constants;
 import com.example.signinsignout.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class userActivity extends AppCompatActivity {
 
@@ -23,6 +31,7 @@ public class userActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
         setListeners();
         getUsers();
@@ -34,11 +43,42 @@ public class userActivity extends AppCompatActivity {
     }
 
     private void getUsers(){
-
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS).get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if(task.isSuccessful() && task.getResult()!=null){
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())){
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            user.id = queryDocumentSnapshot.getId();
+                            user.add(user);
+                        }
+                        if (users.size()>0){
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
     }
 
-    private void showToast(String message){
-        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    private void showErrorMessage(){
+        binding.textErrorMessage.setText(String.format("%s","No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
     }
 
     private void loading(Boolean isLoading){
--- app/src/main/java/com/example/signinsignout/models/User.java
+++ app/src/main/java/com/example/signinsignout/models/User.java
@@ -4,4 +4,7 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     public String name, image, email, token, id;
+
+    public void add(User user) {
+    }
 }
